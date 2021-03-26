e81b68d29ee142ad917278036d23240c09851f43
Ilya <kirmenyov@mail.ru>
2021-03-27 00:48:57 +0300

    added cycles in project

--- js/main.js
+++ js/main.js
@@ -9,13 +9,33 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
+for (let i=0; i<2; ++i) {
+  const  a = prompt("Один из последних просмотренных фильмов?", ''),
+        b = prompt("На сколько оцените его?", '');
+    if (a != null && b != null && a !='' && b != '' && a.length != 50) {
+        console.log("done");
+        personalMovieDB.movies[a] = b;
+    }
+    else {
+        console.log(a);
+        console.log("error");
+        i--;
+    }
+}
+if (personalMovieDB.count < 10){
+    const с = confirm ("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    const d = confirm ("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    const k = confirm("Вы киноман");
+}
+else {
+    const r = confirm("Произошла ошибка");
+}
 
-const a = prompt("Один из последних просмотренных фильмов?", ''),
-     b = prompt("На сколько оцените его?", ''),
-     c = prompt("Один из последних просмотренных фильмов?", ''),
-     d = prompt("На сколько оцените его?", '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+//
+//personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB.movies);
